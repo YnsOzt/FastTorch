47e6da1d457a60636dab67c7aca115091466df7a
YnsOzt <ozturk213@hotmail.fr>
2020-04-14 18:17:35 +0200

    bugfix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
+with open("./README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='fast_torch',
     packages=['fast_torch'],
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='Library that implements boiler plate code in PyTorch for training, testing and plotting your model',
     long_description=long_description,

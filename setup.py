955141162b9efed19b1ab60847c9dd9c30d23b08
YnsOzt <ozturk213@hotmail.fr>
2020-04-14 18:01:56 +0200

    bug fix for pip releases

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='fast_torch',  # How you named your package folder (MyLib)
-    packages=['fast_torch'],  # Chose the same as "name"
+    name='faster_torch',  # How you named your package folder (MyLib)
+    packages=['faster_torch'],  # Chose the same as "name"
     version='0.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Library that implements boiler plate code in PyTorch for training, testing and plotting your model',
@@ -10,7 +10,6 @@ setup(
     author='Sakir Ozturk',  # Type in your name
     author_email='ozturk213@hotmail.fr',  # Type in your E-Mail
     url='https://github.com/YnsOzt/FastTorch',  # Provide either the link to your github or to your website
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
     keywords=['PyTorch', 'boiler_plate', 'Train', 'Test', 'Plot'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'sklearn',
